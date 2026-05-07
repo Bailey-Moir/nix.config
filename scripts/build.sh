2f1768fc4062ef17a6b4a768a8cfd510485c5a28
Bailey-Moir <bailey.p.moir@gmail.com>
2026-05-07 14:50:07 +1200

    I need to automate these messages

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+set -e
+pushd -q /etc/nixos/
+git diff -U0 *.nix
+echo "NixOS Rebuilding..."
+export SUDO_ASKPASS=$(which lxqt-openssh-askpass)
+sudo -A nixos-rebuild switch --impure --flake /etc/nixos#$(hostname) &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
+gen=$(nixos-rebuild list-generations --json | jq '.[] | select(.current==true).generation')
+git commit -am "$(hostname): $gen"
+eww reload
+popd -q
