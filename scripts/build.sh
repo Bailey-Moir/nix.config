24e5da77470c6e72c16e341bdef4bc556ab2e84e
Bailey-Moir <bailey.p.moir@gmail.com>
2026-05-10 15:48:07 +1200

    idk man

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,6 @@ export SUDO_ASKPASS=$(which lxqt-openssh-askpass)
 sudo -A nixos-rebuild switch --impure --flake /etc/nixos#$(hostname) &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
 gen=$(nixos-rebuild list-generations --json | jq '.[] | select(.current==true).generation')
 git commit -am "$(hostname): $gen"
-eww reload
 popd -q
+notify-send "Switched" "NixOS build completed successfully"
+sleep 1 && eww reload &
